bd5cf5822a88bb548ec61dce5bcd627944c4a42a
Gjum <code.gjum@gmail.com>
2016-09-07 20:21:52 +0200

    build 2425545 update renderer

--- bundle.js
+++ bundle.js
@@ -239,7 +239,7 @@ Util.getJSON(dataRoot + 'meta/worlds.json', function (worlds) {
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var attribution = '<a href="https://github.com/CivMap">Civcraft Mapping Agency</a>' + ' | Visit civmap.herokuapp.com' + ' | <a href="https://github.com/CivMap/civmap#contribute">contribute</a>';
+var attribution = '<a href="https://github.com/CivMap">Civcraft Mapping Agency</a>' + ' | Visit civmap.github.io' + ' | <a href="https://github.com/CivMap/civmap#contribute">contribute</a>';
 
 function getJSON(url, onData, onErr) {
   var request = new XMLHttpRequest();
